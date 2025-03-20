7c46f99eea4d93d0ebef002af5450b22735d7217
amit1502 <amitpandey18051996@gmail.com>
2025-03-20 11:01:18 +0530

    adding db config

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,12 +1,16 @@
 package main
 
 import (
+	"context"
+	"database/sql"
 	"flag"
 	"fmt"
 	"log"
 	"net/http"
 	"os"
 	"time"
+
+	_ "github.com/lib/pq"
 )
 
 const version = "1.0.0"
@@ -14,6 +18,12 @@ const version = "1.0.0"
 type config struct {
 	port int
 	env  string
+	db   struct {
+		dsn          string
+		maxOpenConns int
+		maxIdleConns int
+		maxIdleTime  string
+	}
 }
 
 type application struct {
@@ -27,10 +37,25 @@ func main() {
 
 	flag.IntVar(&cfg.port, "port", 8000, "Server port")
 	flag.StringVar(&cfg.env, "env", "development", "Env (production | development | staging")
+	flag.StringVar(&cfg.db.dsn, "db-dsn", "postgres://greenlight:admin@localhost/greenlight", "database dsn")
+
+	flag.IntVar(&cfg.db.maxOpenConns, "db-max-open-conns", 25, "PostgreSQL max open connections")
+	flag.IntVar(&cfg.db.maxIdleConns, "db-max-idle-conns", 25, "PostgreSQL max idle connections")
+	flag.StringVar(&cfg.db.maxIdleTime, "db-max-idle-time", "15m", "PostgreSQL max connection idle time")
 
 	flag.Parse()
 
 	logger := log.New(os.Stdout, "", log.Ldate|log.Ltime)
+
+	db, err := openDB(cfg)
+
+	if err != nil {
+		logger.Fatal(err)
+	}
+
+	defer db.Close()
+
+	logger.Println("Succesfully connected to db")
 
 	app := application{
 		config: cfg,
@@ -48,7 +73,36 @@ func main() {
 	logger.Printf("Starting %s server on port %d\n", app.config.env,
 		app.config.port)
 
-	err := srv.ListenAndServe()
+	err = srv.ListenAndServe()
 	logger.Fatal(err)
 
+}
+
+func openDB(cfg config) (*sql.DB, error) {
+	db, err := sql.Open("postgres", cfg.db.dsn)
+	if err != nil {
+		return nil, err
+	}
+
+	db.SetMaxOpenConns(cfg.db.maxOpenConns)
+
+	db.SetMaxIdleConns(cfg.db.maxIdleConns)
+
+	duration, err := time.ParseDuration(cfg.db.maxIdleTime)
+	if err != nil {
+		return nil, err
+	}
+
+	db.SetConnMaxIdleTime(duration)
+
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+
+	defer cancel()
+
+	err = db.PingContext(ctx)
+	if err != nil {
+		return nil, err
+	}
+
+	return db, nil
 }
--- go.mod
+++ go.mod
@@ -3,3 +3,5 @@ module github.com/amit1502/greenlight
 go 1.18
 
 require github.com/julienschmidt/httprouter v1.3.0
+
+require github.com/lib/pq v1.10.9 // indirect
